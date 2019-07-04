154e88080bf210de8fe5410c43a61ef225f82912
Silvio Willian <willianaika12@gmail.com>
2019-07-04 10:59:51 -0300

    Iniciando estudos de AngularJS

---
+++ front/test.js
@@ -0,0 +1,15 @@
+angular.module('Test', [])
+    .controller('ListaComprasController', ['$scope', function($scope){
+
+        $scope.itens = [
+            {produto: 'Leite', quantidade: 2, comprado: false},
+            {produto: 'Cerveja', quantidade: 12, comprado: false}
+        ];
+    
+        $scope.adicionaItem = function () {
+            $scope.itens.push({produto: $scope.item.produto,
+                               quantidade: $scope.item.quantidade,
+                               comprado: false});
+            $scope.item.produto = $scope.item.quantidade = '';
+        }
+}]);
